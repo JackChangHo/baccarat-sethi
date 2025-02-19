056e456b10a8044c3959fc5709f4b01fb3f9082b
unknown <0963488492xx@gmail.com>
2025-02-19 19:15:46 +0800

    測試自動部屬

---
+++ app.js
@@ -0,0 +1,7 @@
+function predictBaccarat() {
+    document.getElementById("prediction").innerText = "下一局預測：莊";
+}
+
+function predictSethi() {
+    document.getElementById("prediction").innerText = "最佳選台：台 3";
+}
